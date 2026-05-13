378a7d2a7b2914159a715b429459ad6e5e38cd94
Sahar <sahar@beyondmediamarketing.com>
2026-05-13 17:39:23 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxtjs/google-fonts', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/google-fonts', '@vueuse/nuxt', '@nuxtjs/partytown'],
+
+  partytown: {
+    forward: ['dataLayer.push'],
+  },
 
   components: [
     { path: '~/components', pathPrefix: false },
@@ -46,6 +50,7 @@ export default defineNuxtConfig({
       script: [
         {
           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TQBVGHZX');`,
+          type: 'text/partytown',
           tagPriority: 'critical',
         },
       ],
