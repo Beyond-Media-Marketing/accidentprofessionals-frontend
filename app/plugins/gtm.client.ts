0372727df3e0a44f7d6875a3c806c681f132fd45
Sahar <sahar@beyondmediamarketing.com>
2026-05-15 19:35:08 +0300

    Test GTM

--- app/plugins/gtm.client.ts
+++ app/plugins/gtm.client.ts
@@ -1,5 +1,7 @@
 export default defineNuxtPlugin(() => {
   window.addEventListener('load', () => {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
     const script = document.createElement('script')
     script.async = true
     script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-TQBVGHZX'
